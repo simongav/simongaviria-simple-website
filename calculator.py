a52342f3188e62707d6b266654a8497e7173db5e
simongav <simongav@yahoo.com>
2019-01-19 13:34:49 -0500

    adding the linked in link

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add(a,b):
+	return a + b
+def substract(a,b):
+	return a - b
+def multiply(a,b):
+	return a * b
+def divide (a,b):
+	return a / b
+
+print("I'm going use the calculator functions to multiply 5 and 6")
+x = multiply(5,6)
+print(x)
